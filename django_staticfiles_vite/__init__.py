992824681b02a61cebcc1ff58cbd484f2543aa05
jrodriguez <jorge.rodriguezflores@kenmei.ai>
2021-12-13 15:14:37 +0100

    Bump version: 0.0.0 → 0.1.0

--- django_staticfiles_vite/__init__.py
+++ django_staticfiles_vite/__init__.py
@@ -2,4 +2,4 @@
 Vite server integration with django.contrib.staticfiles.
 """
 
-__version__ = '0.0.0'
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-staticfiles-vite',
-    version='0.0.0',
+    version='0.1.0',
     description='',
     url='https://github.com/jorrete/django-staticfiles-vite/',
     author='Jorge Rodríguez-Flores Esparza',
