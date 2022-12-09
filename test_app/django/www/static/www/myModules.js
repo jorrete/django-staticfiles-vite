e1e65e4cbb299befdc6fb8f860d538936153c61b
jorro <jorro@protonmail.com>
2022-12-09 10:56:42 +0100

    more examples

---
+++ test_app/django/www/static/www/myModules.js
@@ -0,0 +1,16 @@
+export function foo(txt) {
+  console.log(txt);
+}
+
+export function bar(txt) {
+  console.log(txt);
+}
+
+const def = {
+  def: true,
+};
+// function def () {
+//   console.log('def');
+// }
+
+export default def;
