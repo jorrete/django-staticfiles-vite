7c3336f93e268a97f27ddef41de31277b667819b
jorro <jorro@protonmail.com>
2022-12-10 21:11:31 +0100

    Simplify vite configs

--- django_staticfiles_vite/node/build.js
+++ django_staticfiles_vite/node/build.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const djangoStatic = require('./plugin-django-static');
-const { build, defineConfig, mergeConfig, loadConfigFromFile } = require('vite');
+const { build } = require('vite');
 const {
   resolveId,
   isCSS,
@@ -40,85 +40,79 @@ const {
     dependencies.push(...deps);
   }
 
-  const finalConfig = mergeConfig(
-    (await loadConfigFromFile())?.config,
-    {
-      configFile: false,
-      envFile: false,
-      logLevel: 'silent', // since the assets management to avoid inlining is it a hack supress warnings
-      css: {
-        postcss: {
-          plugins: [
-            replace({
-              pattern: STATIC_TOKEN,
-              data: {
-                replaceAll: base,
-              },
-            }),
-          ],
+  await build({
+    envFile: false,
+    logLevel: 'silent', // since the assets management to avoid inlining is it a hack supress warnings
+    css: {
+      postcss: {
+        plugins: [
+          replace({
+            pattern: STATIC_TOKEN,
+            data: {
+              replaceAll: base,
+            },
+          }),
+        ],
+      },
+    },
+    plugins: [
+      {
+        ...djangoStatic({
+          base,
+          paths,
+          addDependicies,
+          command: 'build',
+        }),
+        enforce: 'pre',
+      },
+    ],
+    build: {
+      assetsInlineLimit: 10,
+      emptyOutDir: false,
+      cssCodeSplit: true,
+      outDir,
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'chunk.[name].[hash].js',
+          assetFileNames: ({ name, type, source }) => {
+            if (source === '/* vite internal call, ignore */') {
+              return name;
+            }
+
+            const [alias, path] = paths.find(([alias, path]) => {
+              const relativePath = path.replace(process.cwd(), '').slice(1);
+
+              if (name.startsWith(relativePath + '/')) {
+                return [alias, relativePath];
+              }
+
+              return null;
+            });
+
+            const finalName = join(
+              alias,
+              normalizeCSS(
+                name.replace(path.replace(process.cwd(), '').slice(1), '').slice(1),
+              ),
+            );
+
+            if (buildCSS) {
+              return finalName;
+            }
+
+            return finalName.replace('.css', '.js.css');
+          },
         },
       },
-      plugins: [
-        {
-          ...djangoStatic({
-            base,
-            paths,
-            addDependicies,
-            command: 'build',
-          }),
-          enforce: 'pre',
-        },
-      ],
-      build: {
-        assetsInlineLimit: 10,
-        emptyOutDir: false,
-        cssCodeSplit: true,
-        outDir,
-        rollupOptions: {
-          output: {
-            chunkFileNames: 'chunk.[name].[hash].js',
-            assetFileNames: ({ name, type, source }) => {
-              if (source === '/* vite internal call, ignore */') {
-                return name;
-              }
-
-              const [alias, path] = paths.find(([alias, path]) => {
-                const relativePath = path.replace(process.cwd(), '').slice(1);
-
-                if (name.startsWith(relativePath + '/')) {
-                  return [alias, relativePath];
-                }
-
-                return null;
-              });
-
-              const finalName = join(
-                alias,
-                normalizeCSS(
-                  name.replace(path.replace(process.cwd(), '').slice(1), '').slice(1),
-                ),
-              );
-
-              if (buildCSS) {
-                return finalName;
-              }
-
-              return finalName.replace('.css', '.js.css');
-            },
-          },
-        },
-        lib: {
-          entry: entry,
-          formats: [format],
-          fileName: (_, name) => {
-            return normalizeJS(name);
-          },
+      lib: {
+        entry: entry,
+        formats: [format],
+        fileName: (_, name) => {
+          return normalizeJS(name);
         },
       },
     },
-  );
-
-  await build(defineConfig(finalConfig));
+  });
 
   process.stdout.write(JSON.stringify(Array.from(new Set(dependencies))));
 })()
--- django_staticfiles_vite/node/serve.js
+++ django_staticfiles_vite/node/serve.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const { resolve } = require('path');
-const { defineConfig, createServer, mergeConfig, loadConfigFromFile } = require('vite');
+const { createServer } = require('vite');
 const djangoStatic = require('./plugin-django-static');
 const { STATIC_TOKEN } = require('./utils');
 const replace = require("postcss-replace");
@@ -12,53 +12,47 @@ const {
 } = JSON.parse(process.argv[2] || '{}');
 
 (async () => {
-  const finalConfig = mergeConfig(
-    (await loadConfigFromFile())?.config,
-    {
-      configFile: false,
-      envFile: false,
-      css: {
-        postcss: {
-          plugins: [
-            replace({
-              pattern: /(url\(["\']?)/,
-              data: {
-                replaceAll: `$1http://localhost:${port}`,
-              },
-            }),
-          ],
-        },
-      },
-      plugins: [
-        {
-          ...djangoStatic({
-            base,
-            paths,
-            command: 'serve',
+  const server = await createServer({
+    envFile: false,
+    css: {
+      postcss: {
+        plugins: [
+          replace({
+            pattern: /(url\(["\']?)/,
+            data: {
+              replaceAll: `$1http://localhost:${port}`,
+            },
           }),
-          enforce: 'pre',
-        },
-      ],
-      server: {
-        cors: true,
-        host: true,
-        port,
-        hmr: {
-          host: 'localhost',
-          port,
-        },
-        fs: {
-          strict: true,
-          allow: [
-            ...paths.map(([alias, path]) => path),
-            resolve(process.cwd(), 'node_modules'),
-          ]
-        }
+        ],
       },
     },
-  );
-
-  const server = await createServer(defineConfig(finalConfig));
-  await server.listen()
-  server.printUrls()
+    plugins: [
+      {
+        ...djangoStatic({
+          base,
+          paths,
+          command: 'serve',
+        }),
+        enforce: 'pre',
+      },
+    ],
+    server: {
+      cors: true,
+      host: true,
+      port,
+      hmr: {
+        host: 'localhost',
+        port,
+      },
+      fs: {
+        strict: true,
+        allow: [
+          ...paths.map(([alias, path]) => path),
+          resolve(process.cwd(), 'node_modules'),
+        ]
+      }
+    },
+  });
+  await server.listen();
+  server.printUrls();
 })()
