33832215e182bd9d8350e957a0de91432fc6674d
jorro <jorro@protonmail.com>
2022-12-10 18:47:59 +0100

    Simplify tags

--- django_staticfiles_vite/templatetags/vite.py
+++ django_staticfiles_vite/templatetags/vite.py
@@ -12,32 +12,7 @@ from ..utils import get_bundle_css_name, normalize_extension, path_is_vite_bunld
 register = template.Library()
 
 
-def build_vite_url(name):
-    return f"http://localhost:{VITE_PORT}{settings.STATIC_URL}{name}"
-
-
-@register.simple_tag
-def vite_hrm(*args):
-    if not settings.DEBUG:
-        return ""
-
-    path = f"http://localhost:{VITE_PORT}/@vite/client"
-
-    return mark_safe(f'<script type="module" src="{path}"></script>')
-
-
-@register.simple_tag
 def vite_static(name):
-    """
-    Join the given path with the STATIC_URL setting.
-    Usage::
-        {% static path [as varname] %}
-    Examples::
-        {% static "myapp/css/base.css" %}
-        {% static variable_with_path %}
-        {% static "myapp/css/base.css" as admin_base_css %}
-        {% static variable_with_path as varname %}
-    """
     if not path_is_vite_bunlde(name):
         raise Exception(
             f'Missing vite bundle keyworkd "{VITE_BUNDLE_KEYWORD}" for static path: {name}'
@@ -47,12 +22,13 @@ def vite_static(name):
         name = normalize_extension(name)
         return static(name)
 
-    return build_vite_url(name)
+    return f"http://localhost:{VITE_PORT}{settings.STATIC_URL}{name}"
 
 
 @register.simple_tag
 def vite_script(name, **kwargs):
     """ """
+    hrm = kwargs.get("hrm", True)
     defer = kwargs.get("defer", False)
     style = kwargs.get("style", False) and not settings.DEBUG
     path = vite_static(name)
@@ -60,6 +36,11 @@ def vite_script(name, **kwargs):
     return mark_safe(
         "\n".join(
             [
+                (
+                    f'<script type="module" src="http://localhost:{VITE_PORT}/@vite/client"></script>'
+                    if hrm and settings.DEBUG
+                    else ""
+                ),
                 f'<script src="{path}" type="module"{" defer" if defer else ""}></script>',
                 (
                     f'<link href="{vite_static(get_bundle_css_name(normalize_extension(name)))}" rel="stylesheet">'
@@ -74,10 +55,8 @@ def vite_script(name, **kwargs):
 @register.simple_tag
 def vite_style(name, **kwargs):
     """ """
-    defer = kwargs.get("defer", False)
-
     path = vite_static(name)
 
     return mark_safe(
-        f"<link href=\"{path}\" rel=\"stylesheet\"{' defer' if defer else ''}>"
+        f"<link href=\"{path}\" rel=\"stylesheet\">"
     )
