a97fd218e1cf44411e9191bbb0da021fa5f80dd1
jorro <jorro@protonmail.com>
2022-12-10 20:40:28 +0100

    tidy

--- test_app/vite.config.js
+++ test_app/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   build: {
     minify: false,
   },
-  esbuild: {
-    drop: ['console', 'debugger'],
-  },
+  // esbuild: {
+  //   drop: ['console', 'debugger'],
+  // },
 });
