376cba8d9171730d3642a27d2a49b20d9b692da2
jorro <jorro@protonmail.com>
2022-12-23 11:37:44 +0100

    regex for tests not matchin underscores

--- django_staticfiles_vite/node/plugin-django-static.js
+++ django_staticfiles_vite/node/plugin-django-static.js
@@ -17,7 +17,7 @@ function djangoStatic ({
       resolve: {
         alias: [
           {
-            find: new RegExp('^(?:/w+/tests/)(.*)'),
+            find: new RegExp('^(?:/[\\w_-]+/tests/)(.*)'),
             replacement: '$1',
             async customResolver (id) {
               const match = await resolveId.call(this, id, testPaths);
